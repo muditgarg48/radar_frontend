487377651a6b0bdc3f66b6804592b14ea180297b
muditgarg48 <muditgarg48@gmail.com>
2025-03-30 16:49:11 +0100

    Refactoring the code to create separate AdditonalDocGeneration folder and shifting necessary code to that folder

---
+++ src/AdditionalDocSection/AdditionalDocSection.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+
+export default function AdditionalDocSection({resume, jobDescription, jobTitle, jobCompany, deployment}) {
+
+    const [coverLetter, setCoverLetter] = useState({});
+    const [coverLetterImprovements, setCoverLetterImprovements] = useState([]);
+    
+    const handleCoverLetterGeneration = async () => {
+
+        const formData = new FormData();
+        formData.append('resume', resume);
+        formData.append('jd', jobDescription);
+        formData.append('position', jobTitle);
+        formData.append('company', jobCompany);
+
+        const response = await fetch(deployment+"/generate-cover-letter", {
+        method: "POST",
+        body: formData
+        });
+
+        if (response.ok) {
+        const result = await response.json();
+        console.log("Cover Letter generated:", result.cover_letter);
+        setCoverLetter(result.cover_letter);
+        setCoverLetterImprovements(result.improvements);
+        }
+    }
+    
+    return (
+        <div>
+            <button onClick={handleCoverLetterGeneration}>Generate Cover Letter</button>
+            &nbsp;
+            {coverLetter && <div id="cover-letter">
+                <h3>Cover Letter</h3>
+                <p id="cover-letter-greeting">{coverLetter.greeting}</p>
+                <div id="cover-letter-body">
+                    <p>{coverLetter.opening_paragraph}</p>
+                    <p>{coverLetter.body_paragraph}</p>
+                    <p>{coverLetter.closing_paragraph}</p>
+                </div>
+                <div id="cover-letter-signature">
+                    <p>{coverLetter.sign_off}</p>
+                    <p>{coverLetter.signature}</p>
+                </div>
+            </div>}
+            {coverLetterImprovements &&<div>
+            <u>Improvements for final version:</u>
+            <ul>
+                {coverLetterImprovements.map((improvement, index) => {
+                // const keyword = word.substring(1, word.length - 1);
+                return (<li key={index}>{improvement}</li>)
+                })}
+            </ul>
+            </div>}
+        </div>
+    );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import ResumeSection from "./ResumeSection/ResumeSection.jsx";
+import AdditionalDocSection from "./AdditionalDocSection/AdditionalDocSection.jsx";
 import './App.css';
 
 function App() {
@@ -11,8 +12,6 @@ function App() {
   const [jobCompany, setJobCompany] = useState("");
   const [jobKeywords, setJobKeywords] = useState([]);
   const [jobKeyNotes, setJobKeyNotes] = useState([]);
-  const [coverLetter, setCoverLetter] = useState({});
-  const [coverLetterImprovements, setCoverLetterImprovements] = useState([]);
 
   const deployment = "http://localhost:4000";
 
@@ -36,28 +35,6 @@ function App() {
     }
   };
 
-  const handleCoverLetterGeneration = async () => {
-
-    const formData = new FormData();
-    formData.append('resume', resume);
-    formData.append('jd', jobDescription);
-    formData.append('position', jobTitle);
-    formData.append('company', jobCompany);
-
-    const response = await fetch(deployment+"/generate-cover-letter", {
-      method: "POST",
-      body: formData
-    });
-
-
-    if (response.ok) {
-      const result = await response.json();
-      console.log("Cover Letter generated:", result.cover_letter);
-      setCoverLetter(result.cover_letter);
-      setCoverLetterImprovements(result.improvements);
-    }
-  }
-
   return (
     <div>
       <h1>RADAR: Resume and Job Description Analyzer</h1>
@@ -79,11 +56,11 @@ function App() {
           placeholder="Paste job description here..."
           />
         &nbsp;
-        <button onClick={handleJobDescriptionSubmit}>Process Job Description</button>
-        &nbsp;
         {jobDescription && <div id="jd-preview">
           {jobDescription} 
         </div>}
+        &nbsp;
+        <button onClick={handleJobDescriptionSubmit}>Process Job Description</button>
         &nbsp;
         {jobTitle && jobCompany &&<p>
           <b>Job Details:</b> <br/>
@@ -107,30 +84,13 @@ function App() {
             })}
           </ul>
         </div>}
-        <button onClick={handleCoverLetterGeneration}>Generate Cover Letter</button>
-        &nbsp;
-        {coverLetter && <div id="cover-letter">
-          <h3>Cover Letter</h3>
-          <p id="cover-letter-greeting">{coverLetter.greeting}</p>
-          <div id="cover-letter-body">
-            <p>{coverLetter.opening_paragraph}</p>
-            <p>{coverLetter.body_paragraph}</p>
-            <p>{coverLetter.closing_paragraph}</p>
-          </div>
-          <div id="cover-letter-signature">
-            <p>{coverLetter.sign_off}</p>
-            <p>{coverLetter.signature}</p>
-          </div>
-        </div>}
-        {coverLetterImprovements &&<div>
-          <u>Improvements for final version:</u>
-          <ul>
-            {coverLetterImprovements.map((improvement, index) => {
-              // const keyword = word.substring(1, word.length - 1);
-              return (<li key={index}>{improvement}</li>)
-            })}
-          </ul>
-        </div>}
+        <AdditionalDocSection 
+          resume={resume}
+          jobDescription={jobDescription}
+          jobTitle={jobTitle}
+          jobCompany={jobCompany}
+          deployment={deployment}
+        />
       </div>
     </div>
   );
