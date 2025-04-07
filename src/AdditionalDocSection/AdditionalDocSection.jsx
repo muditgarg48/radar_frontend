54d9533a314dc291fb6e4531a73cfce50c2ea2ac
muditgarg48 <muditgarg48@gmail.com>
2025-04-07 13:51:27 +0100

    Minor wording change

--- src/AdditionalDocSection/AdditionalDocSection.jsx
+++ src/AdditionalDocSection/AdditionalDocSection.jsx
@@ -10,9 +10,9 @@ export default function AdditionalDocSection({resume, jobDescription, jobTitle, 
         <div id="additional-docs" className="section-heading">
             <h2>Additional Documents</h2>
             <p>
-                Please note, you <strong>MUST NOT</strong> use this generated document directly. It has been generated based on your resume and job description via a Generative AI and it bound to have a lot of issues and will lack the personal touch of a human. Recruiters and Hiring Managers are capable of easily identifying AI generated documents.
+                Please note, you <strong>MUST NOT</strong> use these generated documents directly. It has been generated based on your resume and job description via a Generative AI and it bound to have a lot of issues and will lack the personal touch of a human. Recruiters and Hiring Managers are capable of easily identifying AI generated documents.
                 <br/>
-                Instead, use it as a starting point for your own document. There will be some suggestions on how to start to improve this generated document. Use them as your guide to make them your own.
+                Instead, use it as a starting point for your own document. There will be some suggestions on how to start to improve each generated document. Use them as your guide to make them your own.
             </p>
             <CoverLetterSection
                 resume={resume}
