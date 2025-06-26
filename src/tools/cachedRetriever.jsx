22127a2f8bb93688c1431a247443349761c0fbe7
muditgarg48 <muditgarg48@gmail.com>
2025-06-26 16:07:20 +0100

    Cache process debugging

--- src/tools/cachedRetriever.jsx
+++ src/tools/cachedRetriever.jsx
@@ -23,15 +23,19 @@ export default async function cachedRetriever(cacheKey, dataKey, deployment, end
         }
     } else {
         // else initialise the cache to update later on
-        console.log("Couldn't find "+cacheKey+" cache");
+        console.log("Couldn't find "+cacheKey+" cache.");
         cache = {};
+        console.log("Creating a empty cache for "+cacheKey);
+        localStorage.setItem(cacheKey, JSON.stringify(cache));
     }
     // update the cache using newly fetched data since we haven't returned till now
     const response = await retrieveFromEndpoint(deployment, endpoint, payload);
     if (response.ok) {
-        console.log("Cached the newly fetched data for "+dataKey+" within "+cacheKey+" cache");
         cache[dataKey] = response;
+        console.log("Cached the newly fetched data for "+dataKey+" within "+cacheKey+" cache with the following:\n"+response);
         localStorage.setItem(cacheKey, JSON.stringify(cache));
+    } else {
+        console.log("Weird. When fetching data for "+dataKey+" within "+cacheKey+" cache, got response: "+response);
     }
     return response;
 }
