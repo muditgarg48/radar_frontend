2289c626b6fac3e1c51c99c00b42a0fc5e7d2dd6
muditgarg48 <muditgarg48@gmail.com>
2025-06-29 15:55:43 +0100

    Converting the additional documents section to another tab view for better user experience and cleaner UI

--- src/sections/AdditionalDocSection/AdditionalDocSection.jsx
+++ src/sections/AdditionalDocSection/AdditionalDocSection.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import './AdditionalDocSection.css';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 import AdditionalMessageSection from "../AdditionalMessageSection/AdditionalMessageSection.jsx";
 import CoverLetterSection from "../CoverLetterSection/CoverLetterSection.jsx";
@@ -13,9 +14,18 @@ export default function AdditionalDocSection() {
                 <br/>
                 Instead, use it as a starting point for your own document. There will be some suggestions on how to start to improve each generated document. Use them as your guide to make them your own.
             </p>
-            <CoverLetterSection/>
-            &nbsp;
-            <AdditionalMessageSection/>
+            <Tabs>
+                <TabList>
+                    <Tab>Cover Letter</Tab>
+                    <Tab>Additional Message</Tab>
+                </TabList>
+                <TabPanel>
+                    <CoverLetterSection/>
+                </TabPanel>
+                <TabPanel>
+                    <AdditionalMessageSection/>
+                </TabPanel>
+            </Tabs>
         </div>
     );
 }
